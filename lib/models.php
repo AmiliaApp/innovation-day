030e8c2e3232fa55f6958ff9ea738f5ee71bf327
Martin Drapeau <martindrapeau@gmail.com>
2018-05-15 21:48:01 -0400

    close mysqli connection

--- lib/models.php
+++ lib/models.php
@@ -98,9 +98,12 @@
       $stmt->execute();
       $stmt->close();
     }
+    
+    $mysqli->close();
 
     $expire = time()+60*60*24*30;
     setcookie(COOKIE_NAME, $session, $expire, '/');
+
     return $session;
   }
 
